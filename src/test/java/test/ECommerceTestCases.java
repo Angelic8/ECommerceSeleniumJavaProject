ad0730c796b3d54a6076b183434a3d81f7cc1559
belle09 <angeliqueumilda1009@gmail.com>
2021-01-26 14:18:20 +0800

    added gitignore

--- src/test/java/test/ECommerceTestCases.java
+++ src/test/java/test/ECommerceTestCases.java
@@ -55,7 +55,7 @@ public class ECommerceTestCases {
 
     @Test
     public void aTestHomepage() {
-
+        // home
         homepageObj.viewHomepage();
         homepageObj.clickDismissBtnHome();
         homepageObj.clickNavLinks();
